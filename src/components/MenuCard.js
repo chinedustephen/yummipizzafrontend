a8d4def13c908bac8b0e6fa7f10b03cf88a64563
Stephen Nkwocha <chinedustevenkwocha@gmail.com>
2020-05-20 15:42:45 +0100

    Updated menu page to use MenuCard component, loader, and make api call to menu endpoint

---
+++ src/components/MenuCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Button, Col } from "react-bootstrap";
+
+function MenuCard(props) {
+	return (
+		<Col sm={4}>
+			<Card className="menu-card">
+				<Card.Img
+					variant="top"
+					src={props.data.menu_image}
+					className="menu-image"
+				/>
+				<Card.Body>
+					<Card.Title>{props.data.menu_name}</Card.Title>
+					<Card.Text>usd {props.data.menu_price}</Card.Text>
+					<Button
+						variant="primary"
+						onClick={() => props.addToCart(props.data.menu_id)}
+					>
+						Add to cart
+					</Button>
+				</Card.Body>
+			</Card>
+		</Col>
+	);
+}
+
+export default MenuCard;
